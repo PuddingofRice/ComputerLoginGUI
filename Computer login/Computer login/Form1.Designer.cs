d625469b8c04b198ebff0d062d25bd3da9dc8f7e
Oliver Rice <84247630+PuddingofRice@users.noreply.github.com>
2023-02-10 15:29:13 +0000

    Add files via upload

---
+++ Computer login/Computer login/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Computer_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsernameGuess = new System.Windows.Forms.TextBox();
+            this.txtPasswordGuess = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(57, 51);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(59, 83);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUsernameGuess
+            // 
+            this.txtUsernameGuess.Location = new System.Drawing.Point(138, 48);
+            this.txtUsernameGuess.Name = "txtUsernameGuess";
+            this.txtUsernameGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtUsernameGuess.TabIndex = 2;
+            // 
+            // txtPasswordGuess
+            // 
+            this.txtPasswordGuess.Location = new System.Drawing.Point(138, 80);
+            this.txtPasswordGuess.Name = "txtPasswordGuess";
+            this.txtPasswordGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtPasswordGuess.TabIndex = 3;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(82, 120);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(125, 23);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.txtPasswordGuess);
+            this.Controls.Add(this.txtUsernameGuess);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsernameGuess;
+        private System.Windows.Forms.TextBox txtPasswordGuess;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
+
---
+++ Computer login/Computer login/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computer_login
+{
+    public partial class Form1 : Form
+    {
+	public static class Globals
+	{
+		public static int Attempts = 5;
+	}
+        public Form1()
+        {
+            InitializeComponent();
+        }
+	
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+
+	    string Username = txtUsernameGuess.Text;
+        string Password = txtPasswordGuess.Text;
+
+            while (Globals.Attempts > 0)
+            {
+               
+
+                if (Username == "Admin" && Password == "Ppa55word!")
+                {
+                        MessageBox.Show("Access Granted");
+                        Globals.Attempts = 0;
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Access Denied!");
+                    Globals.Attempts -= 1;
+                    return;
+                   
+                }
+                
+            }
+        Application.Exit();
+
+        }
+    }
+}
